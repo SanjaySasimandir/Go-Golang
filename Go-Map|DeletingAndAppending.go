bd8dc2ec9ef5f75456d5cf111bb28e1c01900756
Sanjay S <18615926+SanjaySasimandir@users.noreply.github.com>
2020-01-25 19:44:14 +0530

    Update Go-Map|DeletingAndAppending.go

--- Go-Map|DeletingAndAppending.go
+++ Go-Map|DeletingAndAppending.go
@@ -10,8 +10,8 @@ func main() {
 		"Illinois":   56516,
 		"Texas":      556555,
 	}
-	statePopulations["Georgia"]=5445454
-	delete(statePopulations,"Texas")
+	statePopulations["Georgia"] = 5445454
+	delete(statePopulations, "Texas")
 	fmt.Println(statePopulations["California"])
 	fmt.Println(statePopulations)
 }
