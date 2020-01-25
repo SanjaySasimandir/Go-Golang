f286b9af89c10d0c968323b1d5d429c4c3ea5ec9
Sanjay S <18615926+SanjaySasimandir@users.noreply.github.com>
2020-01-25 19:41:01 +0530

    Create Go-Map|DeletingAndAppending.go

---
+++ Go-Map|DeletingAndAppending.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	statePopulations := map[string]int{
+		"California": 19516,
+		"Illinois":   56516,
+		"Texas":      556555,
+	}
+	statePopulations["Georgia"]=5445454
+	delete(statePopulations,"Texas")
+	fmt.Println(statePopulations["California"])
+	fmt.Println(statePopulations)
+}
