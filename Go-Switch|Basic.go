91c4aac5a3f013599a7aebbcb89329c44be720d0
Sanjay S <18615926+SanjaySasimandir@users.noreply.github.com>
2020-01-26 00:18:51 +0530

    Create Go-Switch|Basic.go

---
+++ Go-Switch|Basic.go
@@ -0,0 +1,50 @@
+The Go Playground   Imports 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	switch 2 {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	}
+}
+
+Two
+
+Program exited.
