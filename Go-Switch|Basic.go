e1e05740db755f23b4c61d316e8702f7920313fd
Sanjay S <18615926+SanjaySasimandir@users.noreply.github.com>
2020-01-26 00:20:20 +0530

    Update Go-Switch|Basic.go

--- Go-Switch|Basic.go
+++ Go-Switch|Basic.go
@@ -1,33 +1,3 @@
-The Go Playground   Imports 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
 package main
 
 import (
@@ -36,15 +6,11 @@ import (
 
 func main() {
 	switch 2 {
-	case 1:
+	case 1,4,5:
 		fmt.Println("One")
-	case 2:
+	case 2,6,7:
 		fmt.Println("Two")
 	case 3:
 		fmt.Println("Three")
 	}
 }
-
-Two
-
-Program exited.
