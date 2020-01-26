b170544f132a00abac70d4339aac3d1e59956b12
Sanjay S <18615926+SanjaySasimandir@users.noreply.github.com>
2020-01-26 19:32:41 +0530

    Create Go-Function|Return.go

---
+++ Go-Function|Return.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := sum(1, 2, 3, 4, 5, 6)
+	fmt.Println("The sum is", s)
+}
+func sum(values ...int) int {
+	fmt.Println(values)
+	sum := 0
+	for i := 0; i < len(values); i++ {
+		sum += values[i]
+	}
+	return sum
+}
