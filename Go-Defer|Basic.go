be6fa86654d49be95c88052582ccbce29c610be9
Sanjay S <18615926+SanjaySasimandir@users.noreply.github.com>
2020-01-26 11:43:11 +0530

    Update Go-Defer|Basic.go

--- Go-Defer|Basic.go
+++ Go-Defer|Basic.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	defer fmt.Println("1") //Defer runs lines last, Priority = last>first
+	defer fmt.Println("1") //Defer runs when a function is about to return, Priority = last>first
 	defer fmt.Println("2")
 	defer fmt.Println("3")
 	defer fmt.Println("4")
